25a4b2abb649bb829af47a9b51a82d1b46c621dd
taserbeat <taserbeat@gmail.com>
2022-10-02 17:35:50 +0900

    feat: ch07 Q学習をニューラルネットワークで実装

---
+++ ch07/q_learning_nn.py
@@ -0,0 +1,116 @@
+if "__file__" in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  # for importing the parent dirs
+
+
+from common.gridworld import GridWorld
+import dezero.layers as L
+import dezero.functions as F
+from dezero import optimizers
+from dezero import Model
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def one_hot(state):
+    HEIGHT, WIDTH = 3, 4
+    vec = np.zeros(HEIGHT * WIDTH, dtype=np.float32)
+    y, x = state
+    idx = WIDTH * y + x
+    vec[idx] = 1.0
+    return vec[np.newaxis, :]
+
+
+class QNet(Model):
+    def __init__(self):
+        super().__init__()
+        self.l1 = L.Linear(100)  # hidden_size
+        self.l2 = L.Linear(4)  # action_size
+
+    def forward(self, x):
+        """
+        順伝播
+        """
+
+        x = F.relu(self.l1(x))
+        x = self.l2(x)
+        return x
+
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.lr = 0.01
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        self.qnet = QNet()
+        self.optimizer = optimizers.SGD(self.lr)
+        self.optimizer.setup(self.qnet)
+
+    def get_action(self, state_vec):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            qs = self.qnet(state_vec)
+            return qs.data.argmax()  # type: ignore
+
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q = np.zeros(1)  # [0.]
+        else:
+            next_qs = self.qnet(next_state)
+            next_q = next_qs.max(axis=1)  # type: ignore
+            next_q.unchain()
+
+        target = self.gamma * next_q + reward
+        qs = self.qnet(state)
+        q = qs[:, action]  # type: ignore
+        loss = F.mean_squared_error(target, q)
+
+        self.qnet.cleargrads()
+        loss.backward()  # type: ignore
+        self.optimizer.update()
+
+        return loss.data  # type: ignore
+
+
+env = GridWorld()
+agent = QLearningAgent()
+
+episodes = 1000
+loss_history = []
+
+for episode in range(episodes):
+    state = env.reset()
+    state = one_hot(state)
+    total_loss, cnt = 0, 0
+    done = False
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done = env.step(action)
+        next_state = one_hot(next_state)
+
+        loss = agent.update(state, action, reward, next_state, done)
+        total_loss += loss
+        cnt += 1
+        state = next_state
+
+    average_loss = total_loss / cnt
+    loss_history.append(average_loss)
+
+
+plt.xlabel('episode')
+plt.ylabel('loss')
+plt.plot(range(len(loss_history)), loss_history)
+plt.show()
+
+# visualize
+Q = {}
+for state in env.states():
+    for action in env.action_space:
+        q = agent.qnet(one_hot(state))[:, action]  # type: ignore
+        Q[state, action] = float(q.data)
+env.render_q(Q)
